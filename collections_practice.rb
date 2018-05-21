90f2a0b7de9ca189c2eceaecb83e93fed8a2a2e5
Josh Stegmeier <josh@steg.me>
2018-05-21 21:05:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,9 +11,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  a = array[1]
-  b = array[2]
-  array[2] = a
-  array[1] = b
+  array[1], array[2] = array[2], array[1]
   array
 end
